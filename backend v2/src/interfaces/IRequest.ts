faf52dc62b3c35567685b4dae58a4d5f256017d2
alexy4744 <alexy4744@gmail.com>
2019-02-20 18:21:06 -0500

    remove duplicate folder

--- backend v2/src/interfaces/IRequest.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from "express";
-import { GridFSBucket } from "mongodb";
-
-export default interface IncomingRequest extends Request {
-  bucket: GridFSBucket;
-}
--- backend v2/src/interfaces/MulterFile.ts
+++
@@ -1,6 +0,0 @@
-import { Readable } from "stream";
-
-export default interface MulterFile extends Express.Multer.File {
-  id: string;
-  stream: Readable;
-}
